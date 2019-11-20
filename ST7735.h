5c4abc0f101e5c448a8a313160637390b2df37b6
Jeff Magee <jeffreymagee@gmail.com>
2019-11-20 07:19:55 +0000

    added colors

--- ST7735.h
+++ ST7735.h
@@ -1,28 +1,6 @@
 #ifndef _ST7735_H
 #define _ST7735_H
 
-#include "canvas.h"
-
-#define ST7735_BLACK       0x0000      /*   0,   0,   0 */
-#define ST7735_NAVY        0x000F      /*   0,   0, 128 */
-#define ST7735_DARKGREEN   0x03E0      /*   0, 128,   0 */
-#define ST7735_DARKCYAN    0x03EF      /*   0, 128, 128 */
-#define ST7735_MAROON      0x7800      /* 128,   0,   0 */
-#define ST7735_PURPLE      0x780F      /* 128,   0, 128 */
-#define ST7735_OLIVE       0x7BE0      /* 128, 128,   0 */
-#define ST7735_LIGHTGREY   0xC618      /* 192, 192, 192 */
-#define ST7735_DARKGREY    0x7BEF      /* 128, 128, 128 */
-#define ST7735_BLUE        0x001F      /*   0,   0, 255 */
-#define ST7735_GREEN       0x07E0      /*   0, 255,   0 */
-#define ST7735_CYAN        0x07FF      /*   0, 255, 255 */
-#define ST7735_RED         0xF800      /* 255,   0,   0 */
-#define ST7735_MAGENTA     0xF81F      /* 255,   0, 255 */
-#define ST7735_YELLOW      0xFFE0      /* 255, 255,   0 */
-#define ST7735_WHITE       0xFFFF      /* 255, 255, 255 */
-#define ST7735_ORANGE      0xFD20      /* 255, 165,   0 */
-#define ST7735_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
-#define ST7735_PINK        0xF81F
-
 class ST7735 {
     public:
         void init(void);
---
+++ colors.h
@@ -0,0 +1,24 @@
+#ifndef _COLORS_H
+#define _COLORS_H
+
+#define FFFGFX_BLACK       0x0000      /*   0,   0,   0 */
+#define FFFGFX_NAVY        0x000F      /*   0,   0, 128 */
+#define FFFGFX_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define FFFGFX_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define FFFGFX_MAROON      0x7800      /* 128,   0,   0 */
+#define FFFGFX_PURPLE      0x780F      /* 128,   0, 128 */
+#define FFFGFX_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define FFFGFX_LIGHTGREY   0xC618      /* 192, 192, 192 */
+#define FFFGFX_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define FFFGFX_BLUE        0x001F      /*   0,   0, 255 */
+#define FFFGFX_GREEN       0x07E0      /*   0, 255,   0 */
+#define FFFGFX_CYAN        0x07FF      /*   0, 255, 255 */
+#define FFFGFX_RED         0xF800      /* 255,   0,   0 */
+#define FFFGFX_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define FFFGFX_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define FFFGFX_WHITE       0xFFFF      /* 255, 255, 255 */
+#define FFFGFX_ORANGE      0xFD20      /* 255, 165,   0 */
+#define FFFGFX_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
+#define FFFGFX_PINK        0xF81F       /* fix this */
+
+#endif
