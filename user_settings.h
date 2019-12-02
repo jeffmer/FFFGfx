f68a1f5ffdf542317660cd8098d4672577a3b79d
jeffmer <jeffmer@users.noreply.github.com>
2019-12-02 15:32:21 +0000

    Add files via upload

--- user_settings.h
+++ user_settings.h
@@ -3,7 +3,7 @@
 
 // Displays - uncomment the one you want to use
 
-#define IS_ILI9341
+//#define IS_ILI9341
 //#define IS_ST7735
 
 
@@ -13,13 +13,13 @@
 //int const _YSIZE = 80, _XSIZE = 160, _YOFF = 24, _XOFF = 0, _INVERT = 0, _ROTATE = 0;
 
 //AliExpress 0.96" 80x160 display
-//static int const _YSIZE = 80, _XSIZE = 160, _YOFF = 26, _XOFF = 1, _INVERT = 1, _ROTATE = 2;
+static int const _YSIZE = 80, _XSIZE = 160, _YOFF = 26, _XOFF = 1, _INVERT = 1, _ROTATE = 2;
 
 // Adafruit 1.44" 128x128 display
 //static int const _YSIZE = 128, _XSIZE = 128, _YOFF = 3, _XOFF = 2, _INVERT = 0, _ROTATE = 5;
 
 // Adafruit 1.8" 128x160 display LANDSCAPE
-static int const _YSIZE = 128, _XSIZE = 160, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 2;
+//static int const _YSIZE = 128, _XSIZE = 160, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 2;
 
 // Adafruit 1.8" 128x160 display PORTRAIT
 //static int const _YSIZE = 160, _XSIZE = 128, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 1;
@@ -34,7 +34,7 @@ static int const _YSIZE = 128, _XSIZE = 160, _YOFF = 0, _XOFF = 0, _INVERT = 0, 
 //static int const _YSIZE = 320, _XSIZE = 240, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 3;
 
 //ILI9341 2.2" 240X320 DISPLAY LANDSCAPE
-static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 0;
+//static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 0;
 
 //ILI9341 MSTACK 240X320 DISPLAY LANDSCAPE
 //static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 1;
@@ -43,13 +43,14 @@ static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, 
 //processor board definitions - select one
 //#define _ATMEGA1284_
 //#define _UCHIP_
-#define _FEATHER_M0_
+//#define _FEATHER_M0_
 //#define _FEATHER_M4_
-//#define _NODEMCU_ESP8266_
+#define _NODEMCU_ESP8266_
 //#define _M5STACK_
 //#define _TTGO_ESP32_WROVER_
 
 #ifdef _ATMEGA1284_
+    #define _FFF_CPU_NAME "ATMega1284"
     // Pins for SPI
     #define _DC 16
     #define _CS 15
@@ -60,7 +61,6 @@ static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, 
     // SPI control pins
     #define _DC 2
     #define _CS 7
-    #define _RESET 3
     // SERCOM for DMA to SPI
     #define USE_DMA_FOR_M0
     #define USE_SERCOM SERCOM1             
@@ -80,6 +80,8 @@ static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, 
 
 #ifdef _FEATHER_M4_
     #define _FFF_CPU_NAME "M4 (dma)"
+    #define IS_ILI9341
+    static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 0;
     // SPI control pins
     #define _DC 10
     #define _CS 9
@@ -92,10 +94,11 @@ static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, 
 
 #ifdef _NODEMCU_ESP8266_
     #define _FFF_CPU_NAME "ESP8266 (160mhz)"
+    #define IS_ILI9341
+    static int const _YSIZE = 240, _XSIZE = 320, _YOFF = 0, _XOFF = 0, _INVERT = 0, _ROTATE = 0;
     // Pins for SPI
     #define _DC D4
     #define _CS D1
-    #define _RESET D0
     #define _SPI_WRITEBYTES_
 #endif
 
